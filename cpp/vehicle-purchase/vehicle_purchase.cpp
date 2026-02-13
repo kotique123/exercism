184782dd4ffc75c56ec2ef326a0d8ea0088b2cfc
Daniil Murashov <daniil.murashov@my.tccd.edu>
2026-02-12 22:19:44 -0600

    Completed vehicle_purchase.cpp

--- cpp/vehicle-purchase/vehicle_purchase.cpp
+++ cpp/vehicle-purchase/vehicle_purchase.cpp
@@ -6,6 +6,7 @@ namespace vehicle_purchase {
 // vehicle. Only "car" and "truck" require a license.
 bool needs_license(std::string kind) {
     // TODO: Return true if you need a license for that kind of vehicle.
+    if(kind == "truck" || kind == "car") {return true;}
     return false;
 }
 
@@ -13,14 +14,17 @@ bool needs_license(std::string kind) {
 // vehicle that comes first in lexicographical order.
 std::string choose_vehicle(std::string option1, std::string option2) {
     // TODO: Return the final decision in a sentence.
-    return "not yet implemented";
+    if (option1 < option2) {return option1 + " is clearly the better choice.";}
+    return option2 + " is clearly the better choice.";
 }
 
 // calculate_resell_price calculates how much a vehicle can resell for at a
 // certain age.
 double calculate_resell_price(double original_price, double age) {
     // TODO: Return the age-corrected resell price.
-    return 0.0;
+    if (age < 3) { return original_price * 0.8;}
+    else if (age >= 10) {return original_price * 0.5;}
+    return original_price * 0.7;
 }
 
 }  // namespace vehicle_purchase
